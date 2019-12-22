1e9ec6b774a9a4c69ac2179cc73ba0e2d0c676c9
u admin <admin@test.com>
2019-12-22 16:04:23 +0900

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-2_Wh0zkMuIv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '736i6905d44nv76v0ofvpd3mee', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
